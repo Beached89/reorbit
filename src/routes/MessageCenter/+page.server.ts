519f455e203e06d9a83250204b4e0dfff0c99953
Christine <49208186+ckemme@users.noreply.github.com>
2024-06-21 18:45:53 -0400

    Message Center display DB data
    Sets the page.server.ts to use async await for the db.all and returns the data in the ctoonLibrary
    
    Updates the page.svelte to show the ctoon image and display name

--- src/routes/MessageCenter/+page.server.ts
+++ src/routes/MessageCenter/+page.server.ts
@@ -1,18 +1,24 @@
 import type { Load } from "@sveltejs/kit";
+import type { ctoonLibrary } from '$lib/types.ts';
 import sqlite3 from "sqlite3";
 
 
-export const load = () => {
-        const db = new sqlite3.Database('./Database/BillyMojojojoFinn.db' ,sqlite3.OPEN_READWRITE,(err)=>{
-            if (err) return console.error(err.message);
+export const load: Load = async () => {
+
+        var loadDataPromise = new Promise<ctoonLibrary[]>((resolve, reject) => {
+            var db = new sqlite3.Database('./Database/BillyMojojojoFinn.db' ,sqlite3.OPEN_READWRITE,(err)=>{
+                if (err) return console.error(err.message);
+            });
+            var sql = 'SELECT * FROM Collection';
+            db.all<ctoonLibrary>(sql, (err: Error|null, rows: ctoonLibrary[]) => {
+                if (err) {
+                    reject(err);
+                    return;
+                }
+                resolve(rows)
+            });
         });
- 
-        var sql = 'SELECT * FROM Collection';
-        db.all(sql,[], (err,data) => {
-            if (err) return console.error(err.message);     
-            //console.log(data);
-            return JSON.stringify(data)
-        }); 
+        var rows = await loadDataPromise;
 
-
+        return {'message': rows};
 }
--- src/routes/MessageCenter/+page.svelte
+++ src/routes/MessageCenter/+page.svelte
@@ -1,7 +1,17 @@
 <script>
     /** @type {import('./$types').PageData} */
     export let data;
-    console.log(data.message[0])
+    const ctoon = data.message;
+    console.log(data.message)
 </script>
 
-
+<div class="messagecenter">
+    {#if ctoon.length > 0}
+        {#each ctoon as toon}
+            <div>
+                <img src="{toon.img_url}"/>
+                <p style="color: white">{toon.display_name}</p>
+            </div>
+        {/each}
+    {/if}
+</div>
